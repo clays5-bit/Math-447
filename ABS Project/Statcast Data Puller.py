3ca556ad0a12926c2e76e9c90f4a070322bc996e
SeanOnePointOh <claysean@ymail.com>
2026-05-04 21:25:58 -0700

    Random Updates
    Pitch Data to 5/3/26

--- ABS Project/Statcast Data Puller.py
+++ ABS Project/Statcast Data Puller.py
@@ -6,12 +6,11 @@ import pandas as pd
 import Baseball_Funcs as bf
 import statsapi as stp
 
-data_path = "C:/Users/Sean's Desktop/Documents/GitHub/Math-447/ABS Project/pitch_data_current.csv"
+data_path = "C:/Users/Sean's Desktop/Documents/GitHub/Math-447/pitch_data_current.csv"
 mlb_2026_start_date =  "2026-03-25"
 
 start_date = str(date.today()-timedelta(days=2)) 
 end_date = str(date.today()-timedelta(days=1))
 
-#bf.pull_full_pitch(data_path, mlb_2026_start_date, end_date)
-bf.pull_yesterday_pitch(data_path, start_date, end_date)
-
+bf.pull_full_pitch(data_path, mlb_2026_start_date, end_date)
+#bf.pull_yesterday_pitch(data_path, start_date, end_date)
